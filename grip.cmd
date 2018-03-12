61de4e767dcdb5d3f1049c20ca2df1b5bee28ffc
Halldor Fannar <hfannar@nvidia.com>
2018-03-12 14:44:49 -0700

    Chaned grip.cmd to use packman python directly
    This fixes SSL issue (GitHub has recently upgraded their SSL, requiring newer python which packman provides).

--- grip.cmd
+++ grip.cmd
@@ -1,4 +1,4 @@
 @echo off
 :: Pass in -h or --help for command line options
 :: Usage: grip <input MD file> --export {|output HTML file}
-%PM_PACKAGES_ROOT%\python\2.7.6-windows-x86\python.exe %~dp0\main.py %*
+%PM_PYTHON% %~dp0\main.py %*
